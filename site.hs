e25f3259186ec809d03ed7516e44a51c03281c17
Samuli Thomasson <simsaladin@paivola.fi>
2014-10-14 23:09:49 +0300

    favicons and color enhance logo

--- site.hs
+++ site.hs
@@ -16,6 +16,10 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "favicons/*" $ do
+        route idRoute
+        compile copyFileCompiler
+
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
